6f5c5cc21e442d1cb0fe6adc6e1e8feb1ba07a56
collinsmezie <collinschimezieobinwa@gmail.com>
2022-04-19 13:45:03 +0400

    Add dev branch

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
-import _ from 'lodash';
-import './style.css';
-
-
-
-function component() {
-    const element = document.createElement('div');
+let x = async() => {
+  const response = await fetch("https://themealdb.com/api/json/v1/1/categories.php")
+    
+  return response.json()
   
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+}
 
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
-  
+console.log(x())
