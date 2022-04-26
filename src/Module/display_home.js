baace59fbf68444a891ae1f9e86f1f54b14cdff2
collinsmezie <collinschimezieobinwa@gmail.com>
2022-04-26 16:25:44 +0400

    add like update feature

--- src/Module/display_home.js
+++ src/Module/display_home.js
@@ -1,9 +1,27 @@
-import { addLikes, displayLikes } from './create_likes.js';
+import { addLikes, displayLikes, getAllLikes } from './create_likes.js';
 
 const url2 = 'https://themealdb.com/api/json/v1/1/categories.php';
 const show = document.querySelector('.display');
 
+
+const check = async (item, paragraph) => {
+  if(paragraph === undefined){
+    paragraph = document.querySelectorAll('.likeShow');
+    const fetchLikes = await getAllLikes();
+    fetchLikes.forEach((like) => {
+      if (like.item_id === item) {
+        paragraph.forEach((card) => {
+          if (card.id === item) {
+              card.innerHTML = `${like.likes}`
+          }
+        })
+      }
+    })
+  }
+}
+
 const display = async () => {
+  
   const data = await fetch(url2).then((response) => response.json());
   data.categories.forEach((element) => {
     const meal = document.createElement('div');
@@ -22,19 +40,16 @@ const display = async () => {
       </div>
       <button class="comment-btn" id=${element.idCategory}>Comment</button>
       </div>`;
-    show.appendChild(meal);
-  });
 
+    show.appendChild(meal);
+  })
+  
   const likes = document.querySelectorAll('.fa.fa-thumbs-up');
-  const likeShow = document.querySelectorAll('.likeShow');
   likes.forEach((like) => {
-    like.addEventListener('click', async () => {
-      likeShow.forEach((p) => {
-        if (p.id === like.id) {
-          addLikes(like.id);
-          displayLikes(like.id, p);
-        }
-      });
+      check(like.id)
+      like.addEventListener('click', async () => {
+          await addLikes(like.id); 
+          check(like.id)
     });
   });
 };
